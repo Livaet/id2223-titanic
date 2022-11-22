ef3d5130e3abbdd770e0d0b2942ed5a75a999fdb
Livaet <ekonoja@kth.se>
2022-11-22 22:23:36 +0100

    Done first round

--- titanic-feature-pipeline.py
+++ titanic-feature-pipeline.py
@@ -3,18 +3,62 @@ import modal
 #import great_expectations as ge
 import hopsworks
 import pandas as pd
+import seaborn as sns
 
 project = hopsworks.login()
 fs = project.get_feature_store()
 
-iris_df = pd.read_csv("https://repo.hops.works/master/hopsworks-tutorials/data/iris.csv")
+titanic_df = pd.read_csv("titanic_model/titanic.csv")
 
-iris_fg = fs.get_or_create_feature_group(
-    name="iris_modal",
+#print(titanic_df["Sex"].value_counts(dropna=False))
+
+
+# Fill in missing data in Age feature
+mask = (titanic_df['Pclass'] == 1) & (titanic_df['Sex'] == 'male')
+avg_filler = titanic_df.loc[mask, 'Age'].mean()
+titanic_df.loc[titanic_df['Age'].isnull() & mask, 'Age'] = avg_filler
+
+mask = (titanic_df['Pclass'] == 2) & (titanic_df['Sex'] == 'male')
+avg_filler = titanic_df.loc[mask, 'Age'].mean()
+titanic_df.loc[titanic_df['Age'].isnull() & mask, 'Age'] = avg_filler
+
+mask = (titanic_df['Pclass'] == 3) & (titanic_df['Sex'] == 'male')
+avg_filler = titanic_df.loc[mask, 'Age'].mean()
+titanic_df.loc[titanic_df['Age'].isnull() & mask, 'Age'] = avg_filler
+
+mask = (titanic_df['Pclass'] == 1) & (titanic_df['Sex'] == 'female')
+avg_filler = titanic_df.loc[mask, 'Age'].mean()
+titanic_df.loc[titanic_df['Age'].isnull() & mask, 'Age'] = avg_filler
+
+mask = (titanic_df['Pclass'] == 2) & (titanic_df['Sex'] == 'female')
+avg_filler = titanic_df.loc[mask, 'Age'].mean()
+titanic_df.loc[titanic_df['Age'].isnull() & mask, 'Age'] = avg_filler
+
+mask = (titanic_df['Pclass'] == 3) & (titanic_df['Sex'] == 'female')
+avg_filler = titanic_df.loc[mask, 'Age'].mean()
+titanic_df.loc[titanic_df['Age'].isnull() & mask, 'Age'] = avg_filler
+#print(titanic_df.isnull().sum())
+
+titanic_df.drop("Cabin", inplace=True, axis=1)
+titanic_df["Embarked"].fillna(value="U", inplace=True)
+
+# Categorical data converted to numerical
+titanic_df["Embarked"].replace(["U","S","C","Q"],[0,1,2,3], inplace=True)
+titanic_df["Sex"].replace(["male","female"],[1,2], inplace=True)
+
+titanic_df["Ticket"] = titanic_df["Ticket"].str.extract(r'(\d+)', expand=False)
+titanic_df["Ticket"] = pd.to_numeric(titanic_df['Ticket'])
+
+
+#print(titanic_df["Ticket"])
+
+
+titanic_fg = fs.get_or_create_feature_group(
+    name="titanic_modal",
     version=1,
-    primary_key=["sepal_length","sepal_width","petal_length","petal_width"], 
-    description="Iris flower dataset")
-iris_fg.insert(iris_df, write_options={"wait_for_job" : False})
+    primary_key=["PassengerId"],
+    description="Titanic dataset")
+titanic_fg.insert(titanic_df, write_options={"wait_for_job" : False})
 
 #expectation_suite = ge.core.ExpectationSuite(expectation_suite_name="iris_dimensions")    
 #value_between(expectation_suite, "sepal_length", 4.5, 8.0)
